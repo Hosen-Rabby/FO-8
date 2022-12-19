d037edf735918451e785dccbfef6468a8984d45f
Hosen Rabby <mdhosen21018@gmail.com>
2022-12-19 23:15:24 +0600

    updates

--- projectsforyou/assets/js/loader.js
+++ projectsforyou/assets/js/loader.js
@@ -3,3 +3,31 @@ window.addEventListener("load", function () {
     loader.style.display = "none";
 })
 
+// var top = document.getElementsByClassName("scrollTop")
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() > 300) {
+//         $('top i').css({
+//             "opacity": "1", "pointer-events": "auto"
+//         });
+//     } else {
+//         $('top i').css({
+//             "opacity": "0", "pointer-events": "none"
+//         });
+//     }
+// })
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()>300){
+	    $('.to_top').fadeIn(250);
+	}
+	else{
+	    $('.to_top').fadeOut(250);
+	}
+    });
+    $('.to_top').click(function(){
+	$('html,body').animate(
+	    {scrollTop:0},400
+	);
+    });
+});
